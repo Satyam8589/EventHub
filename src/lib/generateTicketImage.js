b9578fd3d8ea3e3cfb79f9bdb242ad5cdd6c9f15
Satyam8589 <satyamkumarsingh9898@gmail.com>
2025-11-23 05:32:02 +0530

    Fix deployment timeout

--- src/lib/generateTicketImage.js
+++ src/lib/generateTicketImage.js
@@ -1,4 +1,4 @@
-import { createCanvas, loadImage } from "canvas";
+import { createCanvas, loadImage } from "@napi-rs/canvas";
 
 // Helper function to calculate event duration in days (IST timezone aware)
 function calculateEventDays(event) {
